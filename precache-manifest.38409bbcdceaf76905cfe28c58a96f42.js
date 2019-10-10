548da8c81b2847db97ae300e139aad8a905d30bd
JU5TU5d <vladislav261100@gmail.com>
2019-10-10 11:32:56 +0300

    Updates

---
+++ precache-manifest.38409bbcdceaf76905cfe28c58a96f42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e9a4125ca89e65a8aaa8a750cc225e",
+    "url": "/goit-react-hw02-reader/./index.html"
+  },
+  {
+    "revision": "c906adbbf3808a949175",
+    "url": "/goit-react-hw02-reader/./static/css/main.35017b9f.chunk.css"
+  },
+  {
+    "revision": "57c7d641b3f71641cece",
+    "url": "/goit-react-hw02-reader/./static/js/2.687aa339.chunk.js"
+  },
+  {
+    "revision": "c906adbbf3808a949175",
+    "url": "/goit-react-hw02-reader/./static/js/main.c89ed3db.chunk.js"
+  },
+  {
+    "revision": "24739ef435a106c4024c",
+    "url": "/goit-react-hw02-reader/./static/js/runtime-main.244db32a.js"
+  }
+]);
